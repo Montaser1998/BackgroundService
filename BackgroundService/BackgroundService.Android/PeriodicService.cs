e2310024b11d3d5143942ac30d805292ac845b4d
Montaser1998 <montaser268@gmail.com>
2019-09-15 23:48:45 +0300

    Add project files.

---
+++ BackgroundService/BackgroundService.Android/PeriodicService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace BackgroundService.Droid
+{
+    [Service(Enabled = true)]
+    public class PeriodicService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            var Timer = new Timer(new TimerCallback(TickTimer),
+                                  null,
+                                  1000,
+                                  2000);
+
+            return StartCommandResult.ContinuationMask;
+        }
+        int i = 0;
+        private void TickTimer(object state)
+        {
+            Plugin.LocalNotifications.CrossLocalNotifications.Current.Show($"Local Notification Title + {i}", $"Local Notification + {i}", i);
+            i++;
+        }
+        public override void OnDestroy()
+        {
+            var intent = new Intent(this, typeof(PeriodicService));
+            StartService(intent);
+            //base.OnDestroy();
+        }
+    }
+}
